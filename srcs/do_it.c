9c5fdffbeb48833b5998281dc2d5beb35146d4d5
Mathieu MOULLEC <mmoullec@e1r12p22.42.fr>
2016-09-20 15:36:05 +0200

    fini

--- srcs/do_it.c
+++ srcs/do_it.c
@@ -6,11 +6,19 @@
 /*   By: mmoullec <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/08/23 13:08:00 by mmoullec          #+#    #+#             */
-/*   Updated: 2016/09/12 18:11:03 by mmoullec         ###   ########.fr       */
+/*   Updated: 2016/09/20 15:29:55 by mmoullec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
+
+int		red_button(t_mlx *mlx)
+{
+	free(mlx);
+	mlx = NULL;
+	exit(0);
+	return (0);
+}
 
 void	do_it(void (*funct)(void *))
 {
@@ -26,6 +34,7 @@ void	do_it(void (*funct)(void *))
 	mlx_hook(mlx->win, 4, 1L << 2, key_press, mlx);
 	mlx_hook(mlx->win, 5, 1L << 3, key_release, mlx);
 	mlx_hook(mlx->win, 6, (1L << 6), motion_mouse, mlx);
+	mlx_hook(mlx->win, 17, 1L << 17, red_button, mlx);
 	mlx_hook(mlx->win, KeyPress, KeyPressMask, movement_hook, mlx);
 	mlx_loop(mlx->mlx);
 }
